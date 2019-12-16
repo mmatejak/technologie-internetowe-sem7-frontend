7d96cae65bd98c874bb24b48b72c4a42179c6894
mmatejak <matejak.mariusz@gmail.com>
2019-12-16 17:18:24 +0100

    Dodanie komponentów

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ClientComponent } from './client/client.component';
+import { NotificationComponent } from './notification/notification.component';
+import { ServiceComponent } from './service/service.component';
+import { AddressComponent } from './address/address.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClientComponent,
+    NotificationComponent,
+    ServiceComponent,
+    AddressComponent
   ],
   imports: [
     BrowserModule
